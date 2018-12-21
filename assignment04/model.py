d639b4e99734bedce8929b4d922fee73c599289d
MdAsifKhan <mak4086@gmail.com>
2018-12-21 07:30:22 +0100

    final model

--- assignment04/model.py
+++ assignment04/model.py
@@ -9,72 +9,51 @@ from matplotlib import pyplot as plt
 import pdb
 
 class CNN(nn.Module):
-    def __init__(self, pool='max'):
-        super(CNN, self).__init__()
+	def __init__(self, pool='max', drop_p=0):
+		super(CNN, self).__init__()
+		self.drop_p = drop_p
+		self.cnn1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+		self.bn1 = nn.BatchNorm2d(32)
+		self.relu1 = nn.ReLU()
+		self.cnn2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+		self.bn2 = nn.BatchNorm2d(64)
+		self.relu2 = nn.ReLU()
 
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3,
-                              padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu1 = nn.ReLU()
-        self.cnn2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3,
-                              padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU()
-        self.cnn3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3,
-                              padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.relu3 = nn.ReLU()
-        self.cnn4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3,
-                              padding=1)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.relu4 = nn.ReLU()
+		if pool == 'max':
+		    self.pool = nn.MaxPool2d(kernel_size=2)
+		else:
+		    self.pool = nn.AvgPool2d(kernel_size=2)
 
-        self.cnn5 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3,
-                              padding=1)
-        self.bn5 = nn.BatchNorm2d(512)
-        self.relu5 = nn.ReLU()
+		if self.drop_p:
+		    self.drop = nn.Dropout(p=self.drop_p)
+		self.fc1 = nn.Linear(64 * 28 * 23, 512)
+		self.relu6 = nn.ReLU()
+		self.fc2 = nn.Linear(512, 20)
 
-        if pool == 'max':
-            self.pool = nn.MaxPool2d(kernel_size=2)
+		self.loss = nn.CrossEntropyLoss()
 
-        else:
-            self.pool = nn.AvgPool2d(kernel_size=2)
+	def forward(self, x):
+		if self.drop_p:
+			out = self.pool(self.drop(self.relu1(self.bn1(self.cnn1(x)))))
+			out = self.pool(self.drop(self.relu2(self.bn2(self.cnn2(out)))))
+			out = out.view(out.size(0), -1)
+			out = self.fc2(self.drop(self.relu6(self.fc1(out))))
+		else:
+			out = self.pool(self.relu1(self.bn1(self.cnn1(x))))
+			out = self.pool(self.relu2(self.bn2(self.cnn2(out))))
+			out = out.view(out.size(0), -1)
+			out = self.fc2(self.relu6(self.fc1(out)))
+		return out
 
-        self.drop = nn.Dropout(p=0.3)
-        self.fc1 = nn.Linear(512 * 7 * 5, 512)
-        self.relu6 = nn.ReLU()
-        self.fc2 = nn.Linear(512, 10)
+	def cnn1_out(self, x):
+		return self.cnn1(x)
 
-        self.loss = nn.CrossEntropyLoss()
-
-    def forward(self, x):
-
-        out = self.pool(self.drop(self.relu1(self.bn1(self.cnn1(x)))))
-        out = self.pool(self.drop(self.relu2(self.bn2(self.cnn2(out)))))
-        out = self.drop(self.relu3(self.bn3(self.cnn3(out))))
-        out = self.pool(self.drop(self.relu4(self.bn4(self.cnn4(out)))))
-        out = self.pool(self.drop(self.relu5(self.bn5(self.cnn5(out)))))
-
-        out = out.view(out.size(0), -1)
-        out = self.fc2(self.drop(self.relu6(self.fc1(out))))
-        return out
-
-    def cnn1_out(self, x):
-        return self.cnn1(x)
-
-    def cnn2_out(self, x):
-        out = self.pool(self.drop(self.relu1(self.bn1(self.cnn1(x)))))
-        return self.cnn2(out)
-
-    def cnn3_out(self, x):
-        out = self.cnn2_out(x)
-        out = self.pool(self.drop(self.relu2(self.bn2(out))))
-        return self.cnn3(out)
-
-    def cnn4_out(self, x):
-        out = self.cnn3_out(x)
-        out = self.pool(self.drop(self.relu3(self.bn3(out))))
-        return self.cnn4(out)
+	def cnn2_out(self, x):
+		if self.drop_p:
+			out = self.pool(self.drop(self.relu1(self.bn1(self.cnn1(x)))))
+		else:
+			out = self.pool(self.relu1(self.bn1(self.cnn1(x))))
+		return self.cnn2(out)
 
 class ModelEvaluator:
 	def __init__(self, model, epochs, lr, batch_size, use_gpu=False, optim='adam', reg=None):
@@ -95,6 +74,7 @@ class ModelEvaluator:
 		self.train_iter_loss = []
 		self.reg = reg
 		self.batch_size = batch_size
+		self.optim = optim
 		if self.use_gpu:
 			self.model = self.model.cuda()
 
@@ -182,6 +162,13 @@ class ModelEvaluator:
 	def evaluator(self, trainloader, testloader, print_every=1000):
 		for epoch in range(self.epochs):
 			self.train(epoch, trainloader, print_every=print_every)
+			if epoch%5==0:
+					save_model = {'epoch': epoch, 
+						'state_dict': self.model.state_dict(),
+						'optimizer': self.optimizer.state_dict()}
+			model_name = 'Model_lr_{}_opt_{}_epoch_{}'.format(self.lr, self.optim, epoch)
+			model_dir = '../assignment04/model/' + model_name
+			torch.save(save_model, model_dir)			
 			acc_ = self.test(testloader)
 		return acc_
 
